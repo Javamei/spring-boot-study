1f152d25c03c40ca1561416a460757247034cd8e
meikun <330704009@qq.com>
2019-04-15 17:29:01 +0800

    统一异常处理

--- demo/src/main/java/com/example/demo/web/HelloController.java
+++ demo/src/main/java/com/example/demo/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.web;
 
+import com.example.demo.until.MyException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,15 @@ public class HelloController {
     @RequestMapping("/hello")
     public String index(){
         return "hello,meikun!";
+    }
+
+    @RequestMapping("/exception")
+    public String exception() throws Exception{
+        throw new Exception("发生错误");
+    }
+
+    @RequestMapping("/json")
+    public String json() throws MyException {
+        throw new MyException("发生错误2");
     }
 }
